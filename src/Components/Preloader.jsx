cbbaf94125b5a23ce6515fd2c5b981e00a1335de
Jibin Aniyan <jibinaniyan80@gmail.com>
2025-11-24 22:06:06 -0800

    initial push

---
+++ src/Components/Preloader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Preloader() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <img src="https://www.pikpng.com/pngl/b/126-1266040_book-now-and-have-fun-at-skyjumper-book.png" alt="preloader" />
+    </div>
+  )
+}
+
+export default Preloader
